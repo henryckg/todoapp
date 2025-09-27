819fbef0146a5b74aa882fe8861321f7d310627a
Henryck <guaramatohenryck@gmail.com>
2025-09-27 14:59:08 -0300

    Implementar funcionalidad para agregar y eliminar elementos en la lista

--- versions/v2/src/App.jsx
+++ versions/v2/src/App.jsx
@@ -1,29 +1,62 @@
+import { useState } from 'react'
 import './App.css'
 
 export default function App () {
+  const [items, setItems] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const data = new window.FormData(e.target)
+    const item = data.get('item')
+
+    if (item === '') return
+
+    setItems((prev) => (
+      [
+        ...prev,
+        item
+      ]
+    ))
+
+    e.target.reset()
+  }
+
+  const handleDelete = (i) => {
+    const filteredList = items.filter((item, index) => index !== i)
+    setItems(filteredList)
+  }
+
   return (
     <div className='list-card'>
       <header>
         <h1>To Do List</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type='text'
             name='item'
-            placeholder='Introduce producto...'
+            placeholder='Introduce un producto...'
           />
         </form>
       </header>
       <main>
         <section>
           <ul>
-            <li>
-              <span>
-                Texto
-              </span>
-              <button>
-                &times;
-              </button>
-            </li>
+            {
+              items.map((item, i) => (
+                <li key={i}>
+                  <span>
+                    {item}
+                  </span>
+                  <button
+                    className='delete-btn'
+                    onClick={() => handleDelete(i)}
+                  >
+                    &times;
+                  </button>
+                </li>
+              ))
+            }
           </ul>
         </section>
       </main>
