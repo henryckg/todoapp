6dcf07e7536ae8acf411eca4a003510e368c9063
Henryck <guaramatohenryck@gmail.com>
2025-10-09 23:04:22 -0300

    feat: agregar funcionalidad para marcar productos como completados en la lista

--- versions/v3/src/App.jsx
+++ versions/v3/src/App.jsx
@@ -30,6 +30,11 @@ function App () {
     setProducts(filteredItems)
   }
 
+  const handleCheck = (i) => {
+    const updatedList = products.map((item, index) => index === i ? { ...item, completed: !item.completed } : item)
+    setProducts(updatedList)
+  }
+
   return (
     <div className='card-list'>
       <header>
@@ -44,8 +49,14 @@ function App () {
           {
             products.map((item, i) => (
               <li key={i}>
-                <input type='checkbox' />
-                <span>{item.product}</span>
+                <input type='checkbox' onChange={() => handleCheck(i)} />
+                <span
+                  style={{
+                    textDecoration: item.completed ? 'line-through' : 'none'
+                  }}
+                >
+                  {item.product}
+                </span>
                 <button onClick={() => handleDelete(i)}>
                   <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-x'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M18 6l-12 12' /><path d='M6 6l12 12' /></svg>
                 </button>
